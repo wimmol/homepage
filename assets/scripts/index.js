e2c656aef4bdb8c44322fabc877618b901eb6589
Pavel Zaitsev <z.pasha00@gmail.com>
2021-01-13 23:24:23 +0200

    add showMen function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,6 +12,22 @@ function avaOnClick() {
         startGame();
     }
 }
+    function showMen() {
+        let game = document.getElementById('game');
+        let manNum = Math.floor(game.offsetWidth / 30) * Math.floor(game.offsetHeight / 30);
+        for (let i = 0; i < manNum; i++) {
+            game.append(createElement({tagName: 'img', className: 'man'}));
+        }
+        let men = document.getElementsByClassName('man');
+        for (let i = 0; i < men.length; i++) {
+            men[i].setAttribute('src', `assets/images/0${Math.floor(Math.random() * 21 + 1)}.png`);
+        }
+        let resume = document.getElementsByTagName('article')[0];
+        resume.style.visibility = 'hidden';
+        return men;
+    }
+}
+
 function createElement({ tagName, className, attributes = {} }) {
     const element = document.createElement(tagName);
 

8da5b621ec792f2e3a8f65a2e623a35988aa24a8
Pavel Zaitsev <z.pasha00@gmail.com>
2021-01-13 23:22:23 +0200

    add game starter

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,14 @@
+let avaCounter = 0;
 
+setTimeout(()=>{
+    let ava = document.getElementById('p-ava')
+    ava.addEventListener('click', avaOnClick);
+}, 2000);
+
+function avaOnClick() {
+    avaCounter++;
+    setTimeout(()=>avaCounter--, 2000);
+    if (avaCounter > 5) {
+        startGame();
+    }
+}

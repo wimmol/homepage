022a83d5dcee4b76834c791ab50306d6aabc2abb
Pavel Zaitsev <z.pasha00@gmail.com>
2021-01-14 00:02:36 +0200

    lil fix

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -21,7 +21,7 @@ function startGame() {
     let dayTime = 1000;
     let deathCounter = 0;
     let infCoef = 0.2;
-    let startInfCount = 15;
+    let startInfCount = 10;
 
     for (let i = 0; i < startInfCount; i++){
         getSickRandMan();
